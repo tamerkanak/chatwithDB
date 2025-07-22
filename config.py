806af7114772da7d410dca6e02d6481d9cd0ff84
Tamer Kanak <81855382+tamerkanak@users.noreply.github.com>
2025-07-23 02:08:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
+import streamlit as st
 
-QDRANT_HOST = "localhost"
+QDRANT_HOST = "00bf48d1-0936-40be-b23c-76b37ea23486.eu-west-2-0.aws.cloud.qdrant.io"
 QDRANT_PORT = 6333
-DATA_DIR = "./data"  # CSV/XLSX files directory
-GEMINI_API_KEY = os.environ.get("API_KEY")  # API key is read from environment variable
+QDRANT_API_KEY = st.secrets.get("QDRANT_API_KEY") or os.environ.get("QDRANT_API_KEY")
+DATA_DIR = "./data"
+GEMINI_API_KEY = st.secrets.get("API_KEY") or os.environ.get("API_KEY") 
