384d00dce0dae51559301a09766d3bf582ad8d77
Tamer Kanak <81855382+tamerkanak@users.noreply.github.com>
2025-07-23 02:11:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 
-QDRANT_HOST = "00bf48d1-0936-40be-b23c-76b37ea23486.eu-west-2-0.aws.cloud.qdrant.io"
+QDRANT_URL = "https://00bf48d1-0936-40be-b23c-76b37ea23486.eu-west-2-0.aws.cloud.qdrant.io"
 QDRANT_PORT = 6333
 QDRANT_API_KEY = st.secrets.get("QDRANT_API_KEY") or os.environ.get("QDRANT_API_KEY")
 DATA_DIR = "./data"
